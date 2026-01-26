e21c7acbb849a6af455d02ccf6de719df84ea2b4
tharun11 <47290042+DrCheeseFace@users.noreply.github.com>
2026-01-26 13:26:58 +0000

    refactor: changed func names for B_buttons.c

--- src/b_button.c
+++ src/b_button.c
@@ -1,31 +1,30 @@
 #include "b_button.h"
-#include "mr_utils/mrv_vectors.h"
-#include "src/g_gamestate.h"
+#include "g_gamestate.h"
 #include "util.h"
 
 #include <SDL3/SDL.h>
 #include <mr_utils.h>
 #include <stdlib.h>
 
-MrvVector active_buttons;
+MrvVector registered_buttons;
 
-mr_internal B_Button *B_button_exists(const char *id);
-mr_internal void B_button_destroy(size_t idx);
+mr_internal B_Button *B_get_registered_button(const char *id);
+mr_internal void B_registered_button_destroy(size_t idx);
 
 void B_init(void)
 {
-	mrv_init(&active_buttons, 0, sizeof(B_Button));
+	mrv_init(&registered_buttons, 0, sizeof(B_Button));
 
 	return;
 }
 
 void B_destroy(void)
 {
-	for (size_t i = active_buttons.len; i > 0; i--) {
-		B_button_destroy(i - 1);
+	for (size_t i = registered_buttons.len; i > 0; i--) {
+		B_registered_button_destroy(i - 1);
 	}
 
-	mrv_free(&active_buttons);
+	mrv_free(&registered_buttons);
 }
 
 int B_register_button(const char *id_str, SDL_FRect box,
@@ -34,19 +33,20 @@ int B_register_button(const char *id_str, SDL_FRect box,
 		      bool (*destroy_when)(struct G_GameState *gamestate),
 		      void *args)
 {
-	B_Button *button = B_button_exists(id_str);
+	B_Button *button = B_get_registered_button(id_str);
 	if (!button) {
 		B_Button new_button = { 0 };
 		mrs_init(strlen(id_str), id_str, strlen(id_str),
 			 &new_button.id_str);
 
-		if (mrv_append(&active_buttons, &new_button,
+		if (mrv_append(&registered_buttons, &new_button,
 			       APPEND_SCALING_INCREMENT) != OK) {
 			mrs_free(&new_button.id_str);
 			return 1;
 		}
 
-		button = mrv_get_idx(&active_buttons, active_buttons.len - 1);
+		button = mrv_get_idx(&registered_buttons,
+				     registered_buttons.len - 1);
 	}
 
 	button->box = box;
@@ -57,15 +57,15 @@ int B_register_button(const char *id_str, SDL_FRect box,
 	return 0;
 }
 
-void B_active_buttons_purge(struct G_GameState *gamestate)
+void B_registered_buttons_purge_dead(struct G_GameState *gamestate)
 {
 	B_Button *button;
-	for (size_t i = active_buttons.len; i > 0; i--) {
+	for (size_t i = registered_buttons.len; i > 0; i--) {
 		size_t idx = i - 1;
-		button = mrv_get_idx(&active_buttons, idx);
+		button = mrv_get_idx(&registered_buttons, idx);
 
 		if (button->destroy_when && button->destroy_when(gamestate)) {
-			B_button_destroy(idx);
+			B_registered_button_destroy(idx);
 		}
 	}
 }
@@ -73,15 +73,15 @@ void B_active_buttons_purge(struct G_GameState *gamestate)
 bool B_handle_click(struct G_GameState *gamestate, SDL_Renderer *sdl_renderer,
 		    SDL_MouseButtonEvent button_event)
 {
-	B_active_buttons_purge(gamestate);
+	B_registered_buttons_purge_dead(gamestate);
 
 	// searched backwards for most recently added button
 	B_Button *button;
 	size_t idx;
-	for (size_t i = active_buttons.len; i > 0; i--) {
+	for (size_t i = registered_buttons.len; i > 0; i--) {
 		idx = i - 1;
 
-		button = mrv_get_idx(&active_buttons, idx);
+		button = mrv_get_idx(&registered_buttons, idx);
 		if (is_pointer_within_bounds(sdl_renderer, button_event,
 					     button->box)) {
 			button->on_click(gamestate, button->args);
@@ -91,18 +91,18 @@ bool B_handle_click(struct G_GameState *gamestate, SDL_Renderer *sdl_renderer,
 	return false;
 }
 
-mr_internal void B_button_destroy(size_t idx)
+mr_internal void B_registered_button_destroy(size_t idx)
 {
-	B_Button *button_to_destroy = mrv_get_idx(&active_buttons, idx);
+	B_Button *button_to_destroy = mrv_get_idx(&registered_buttons, idx);
 	mrs_free(&button_to_destroy->id_str);
-	mrv_remove(&active_buttons, idx);
+	mrv_remove(&registered_buttons, idx);
 }
 
-mr_internal B_Button *B_button_exists(const char *id)
+mr_internal B_Button *B_get_registered_button(const char *id)
 {
 	B_Button *button;
-	for (size_t i = 0; i < active_buttons.len; i++) {
-		button = mrv_get_idx(&active_buttons, i);
+	for (size_t i = 0; i < registered_buttons.len; i++) {
+		button = mrv_get_idx(&registered_buttons, i);
 		if (strcmp(button->id_str.value, id) == 0) {
 			return button;
 		}
